109564f35b54192f20b398e6fd65cfaecdb7a3bd
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-12 08:18:06 -0700

    feat: add get movie by id query

---
+++ src/Application/Movies/Queries/GetMovieById/GetMovieByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Movies.Queries.GetMovieById;
+public class GetMovieByIdQuery : IRequest<Movie>
+{
+    public int Id { get; set; }
+}
+
+public class GetMovieByIdQueryHandler : IRequestHandler<GetMovieByIdQuery, Movie>
+{
+    private readonly IAppDbContext _appDbContext;
+
+    public GetMovieByIdQueryHandler(IAppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<Movie> Handle(GetMovieByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await _appDbContext.Movies.FindAsync(request.Id);
+
+        if (entity == null)
+        {
+            //TODO: Wrire NotFound Custom Exeption
+            throw new Exception();
+        }
+
+        return entity;
+    }
+}
