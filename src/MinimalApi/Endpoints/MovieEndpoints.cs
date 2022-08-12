cfbf9629cb708acfdeafdb6033b6737a196b48b4
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-12 08:31:10 -0700

    refactor: clear program.cs and move endpoint to seperate class

---
+++ src/MinimalApi/Endpoints/MovieEndpoints.cs
@@ -0,0 +1,76 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dtos;
+
+namespace MinimalApi.Endpoints;
+
+public static class MovieEndpoints
+{
+    public static WebApplication AddMovieEndpoints(this WebApplication app)
+    {
+        app.MapGet("api/v1/movies", async (IAppDbContext context) => {
+            var movies = await context.Movies.ToListAsync();
+            return Results.Ok(movies);
+        });
+
+        app.MapGet("api/v1/movies/{id}", async (IAppDbContext context, int id) => {
+            var movie = await context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                return Results.Ok(movie);
+            }
+            return Results.NotFound();
+        });
+
+        app.MapPost("api/v1/movies", async (IAppDbContext context, MovieDto MovieDto, CancellationToken cancellationToken) => {
+
+            var movie = new Movie()
+            {
+                title = MovieDto.title,
+                description = MovieDto.description,
+                CreatedDate = MovieDto.CreatedDate
+            };
+
+            await context.Movies.AddAsync(movie);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Results.Created($"api/v1/commands/{movie.id}", MovieDto);
+
+        });
+
+        app.MapPut("api/v1/movies/{id}", async (IAppDbContext context, int id, MovieDto MovieDto, CancellationToken cancellationToken) => {
+            var movie = await context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return Results.NotFound();
+            }
+
+            movie.title = MovieDto.title;
+            movie.description = MovieDto.description;
+            movie.CreatedDate = MovieDto.CreatedDate;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Results.NoContent();
+        });
+
+        app.MapDelete("api/v1/movies/{id}", async (IAppDbContext context, int id, CancellationToken cancellationToken) => {
+            var movie = await context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return Results.NotFound();
+            }
+
+            context.Movies.Remove(movie);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Results.NoContent();
+
+        });
+
+        return app;
+    }
+}
--- src/MinimalApi/Program.cs
+++ src/MinimalApi/Program.cs
@@ -4,6 +4,7 @@ using Domain.Entities;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dtos;
+using MinimalApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,66 +27,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("api/v1/movies", async (IAppDbContext context) => {
-    var movies = await context.Movies.ToListAsync();
-    return Results.Ok(movies);
-});
-
-app.MapGet("api/v1/movies/{id}", async (IAppDbContext context, int id) => {
-    var movie = await context.Movies.FindAsync(id);
-    if (movie != null)
-    {
-        return Results.Ok(movie);
-    }
-    return Results.NotFound();
-});
-
-app.MapPost("api/v1/movies", async (IAppDbContext context, MovieDto MovieDto, CancellationToken cancellationToken) => {
-
-    var movie = new Movie()
-    {
-        title = MovieDto.title,
-        description = MovieDto.description,
-        CreatedDate = MovieDto.CreatedDate
-    };
-
-    await context.Movies.AddAsync(movie);
-
-    await context.SaveChangesAsync(cancellationToken);
-
-    return Results.Created($"api/v1/commands/{movie.id}", MovieDto);
-
-});
-
-app.MapPut("api/v1/movies/{id}", async (IAppDbContext context,int id, MovieDto MovieDto, CancellationToken cancellationToken) => {
-    var movie = await context.Movies.FindAsync(id);
-    if (movie == null)
-    {
-        return Results.NotFound();
-    }
-
-    movie.title = MovieDto.title;
-    movie.description = MovieDto.description;
-    movie.CreatedDate = MovieDto.CreatedDate;
-
-    await context.SaveChangesAsync(cancellationToken);
-
-    return Results.NoContent();
-});
-
-app.MapDelete("api/v1/movies/{id}", async (IAppDbContext context,int id, CancellationToken cancellationToken) => {
-    var movie = await context.Movies.FindAsync(id);
-    if (movie == null)
-    {
-        return Results.NotFound();
-    }
-
-    context.Movies.Remove(movie);
-
-    await context.SaveChangesAsync(cancellationToken);
-
-    return Results.NoContent();
-
-});
+app.AddMovieEndpoints();
 
 app.Run();
