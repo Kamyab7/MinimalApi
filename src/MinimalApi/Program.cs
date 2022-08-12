5e455c20b7eb79e14d8e5a2fd0eb7980ec290a62
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-12 04:04:25 -0700

    setup: configuring database

--- src/MinimalApi/Program.cs
+++ src/MinimalApi/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("MinimalApiDb"));
+
+builder.Services.AddScoped<AppDbContext>();
 
 var app = builder.Build();
 
