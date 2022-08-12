10f880b0382a0d3a00be58665ecb30f8675058a7
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-12 07:59:58 -0700

    Feat: add delete movie command

---
+++ src/Application/Movies/Commands/DeleteMovie/DeleteMovieCommand.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Movies.Commands.DeleteMovie;
+public class DeleteMovieCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
+
+public class DeleteMovieCommandHandler : IRequestHandler<DeleteMovieCommand, Unit>
+{
+    private readonly IAppDbContext _appDbContext;
+
+    public DeleteMovieCommandHandler(IAppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<Unit> Handle(DeleteMovieCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _appDbContext.Movies.FindAsync(request.Id);
+
+        if (entity==null)
+        {
+            //TODO: Write NotFound Custom Exeption
+            throw new Exception();
+        }
+
+        _appDbContext.Movies.Remove(entity);
+
+        await _appDbContext.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
