e5fdb45c5d6cd6265287a4626f62429e24daeb1d
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-20 00:01:39 -0700

    setup: nswag config

--- src/MinimalApi/Dtos/MovieDto.cs
+++ src/MinimalApi/Dtos/MovieDto.cs
@@ -2,9 +2,9 @@
 
 public class MovieDto
 {
-    public string title { get; set; }
+    public string? title { get; set; }
 
-    public string description { get; set; }
+    public string? description { get; set; }
 
     public DateTime CreatedDate { get; set; }
 }
--- src/MinimalApi/Program.cs
+++ src/MinimalApi/Program.cs
@@ -2,31 +2,51 @@ using Microsoft.EntityFrameworkCore;
 using MinimalApi.Database;
 using MinimalApi.Dtos;
 using MinimalApi.Entities;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("MinimalApiDb"));
 
 builder.Services.AddScoped<AppDbContext>();
 
+builder.Services.AddOpenApiDocument(configure =>
+{
+    configure.Title = "Minimal API";
+    configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+    {
+        Type = OpenApiSecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = OpenApiSecurityApiKeyLocation.Header,
+        Description = "Type into the textbox: Bearer {your JWT token}."
+    });
+
+    configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseSwaggerUi3(settings =>
+{
+    settings.Path = "/api";
+    settings.DocumentPath = "/api/specification.json";
+});
 
 app.MapGet("api/v1/movies", async (AppDbContext context) => {
     var movies = await context.Movies.ToListAsync();
