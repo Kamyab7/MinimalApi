1f989415f1d4b485479b5d174ab0f5d53efe3acb
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-22 00:32:43 -0700

    refactor: use mediator in api endpoints

--- src/MinimalApi/Endpoints/MovieEndpoints.cs
+++ src/MinimalApi/Endpoints/MovieEndpoints.cs
@@ -1,5 +1,11 @@
 ﻿using Application.Common.Interfaces;
+using Application.Movies.Commands.CreateMovie;
+using Application.Movies.Commands.DeleteMovie;
+using Application.Movies.Commands.UpdateMovie;
+using Application.Movies.Queries.GetAllMovie;
+using Application.Movies.Queries.GetMovieById;
 using Domain.Entities;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dtos;
 
@@ -9,66 +15,24 @@ public static class MovieEndpoints
 {
     public static WebApplication AddMovieEndpoints(this WebApplication app)
     {
-        app.MapGet("api/v1/movies", async (IAppDbContext context) => {
-            var movies = await context.Movies.ToListAsync();
-            return Results.Ok(movies);
+        app.MapGet("api/v1/movies", async (IMediator mediator) => {
+            return await mediator.Send(new GetAllMovieQuery());
         });
 
-        app.MapGet("api/v1/movies/{id}", async (IAppDbContext context, int id) => {
-            var movie = await context.Movies.FindAsync(id);
-            if (movie != null)
-            {
-                return Results.Ok(movie);
-            }
-            return Results.NotFound();
+        app.MapGet("api/v1/movies/{id}", async(int id,IMediator mediator) => {
+            return await mediator.Send(new GetMovieByIdQuery() {Id=id });
         });
 
-        app.MapPost("api/v1/movies", async (IAppDbContext context, MovieDto MovieDto, CancellationToken cancellationToken) => {
-
-            var movie = new Movie()
-            {
-                title = MovieDto.title,
-                description = MovieDto.description,
-                CreatedDate = MovieDto.CreatedDate
-            };
-
-            await context.Movies.AddAsync(movie);
-
-            await context.SaveChangesAsync(cancellationToken);
-
-            return Results.Created($"api/v1/commands/{movie.id}", MovieDto);
-
+        app.MapPost("api/v1/movies", async (CreateMovieCommand command,IMediator mediator) => {
+            return await mediator.Send(command);
         });
 
-        app.MapPut("api/v1/movies/{id}", async (IAppDbContext context, int id, MovieDto MovieDto, CancellationToken cancellationToken) => {
-            var movie = await context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return Results.NotFound();
-            }
-
-            movie.title = MovieDto.title;
-            movie.description = MovieDto.description;
-            movie.CreatedDate = MovieDto.CreatedDate;
-
-            await context.SaveChangesAsync(cancellationToken);
-
-            return Results.NoContent();
+        app.MapPut("api/v1/movies/{id}", async (int id, UpdateMovieCommand command, IMediator mediator) => {
+            return await mediator.Send(command);
         });
 
-        app.MapDelete("api/v1/movies/{id}", async (IAppDbContext context, int id, CancellationToken cancellationToken) => {
-            var movie = await context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return Results.NotFound();
-            }
-
-            context.Movies.Remove(movie);
-
-            await context.SaveChangesAsync(cancellationToken);
-
-            return Results.NoContent();
-
+        app.MapDelete("api/v1/movies/{id}", async (int id,IMediator mediator) => {
+            return await mediator.Send(new DeleteMovieCommand() {Id=id });
         });
 
         return app;
