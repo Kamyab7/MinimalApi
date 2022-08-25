63053ae19795a9f0d8d122b945dbe84a5cb34ea5
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-25 01:09:23 -0700

    setup: initial integeration test project

---
+++ tests/Application.IntegerationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Application.IntegerationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
