175ac7dcdd11e7c76132ddcfac54ae76698b2854
Kamyab Valipour <51748383+Kamyab7@users.noreply.github.com>
2022-08-29 02:10:21 -0700

    feat: Write CreateMovieCommandValidator

---
+++ src/Application/Movies/Commands/CreateMovie/CreateMovieCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Movies.Commands.CreateMovie
+{
+    public class CreateMovieCommandValidator : AbstractValidator<CreateMovieCommand>
+    {
+        private readonly IAppDbContext _context;
+
+        public CreateMovieCommandValidator(IAppDbContext context)
+        {
+            _context = context;
+
+            RuleFor(v => v.title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(200).WithMessage("Title must not exceed 200 characters.");
+
+            RuleFor(v => v.description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(v => v.CreatedDate)
+                .NotEmpty().WithMessage("CreatedDate is required.");
+        }
+    }
+}
